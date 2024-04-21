c6f68e4b2f043ecad8f1e19233dc54d50777935e
Calvin <calvinalvito19@gmail.com>
2024-04-21 19:54:51 +0700

    Challenge3

---
+++ scripts/sortCarByYearAscendingly.js
@@ -0,0 +1,17 @@
+function sortCarByYearAscendingly(cars) {
+  console.log(cars);
+
+  const result = [...cars];
+
+  for (let i = 0; i < result.length - 1; i++) {
+    for (let j = 0; j < result.length - i - 1; j++) {
+      if (result[j].year > result[j + 1].year) {
+        let temp = result[j];
+        result[j] = result[j + 1];
+        result[j + 1] = temp;
+      }
+    }
+  }
+
+  return result;
+}
